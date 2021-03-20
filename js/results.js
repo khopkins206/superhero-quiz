0d01a90679d42d7472cfab5647e678b3fca95bc3
Khawulani Thebe <khawuza@yahoo.com>
2021-03-20 13:54:43 -0500

    Adding the video results

--- js/results.js
+++ js/results.js
@@ -22,3 +22,32 @@ var imgEl = document.getElementById("Gif")
       };
 
       getSuperHero(superhero);
+
+      function getVideo() {
+        $.ajax({
+          type: 'GET',
+          url: 'https://www.googleapis.com/youtube/v3/search',
+          data: {
+              key: 'AIzaSyC30CwUT3GBRL3Gc05HvVMVdAc4MmwHgGI',
+              q: "Black Widow Super Hero",
+              part: 'snippet',
+              maxResults: 2,
+              type: 'video',
+              videoEmbeddable: true,
+          },
+          success: function(data){
+              embedVideo(data)
+          },
+          error: function(response){
+              console.log("Request Failed");
+          }
+        });
+      }
+    
+      function embedVideo(data) {
+        $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
+        $('h3').text(data.items[0].snippet.title)
+        $('.description').text(data.items[0].snippet.description)
+    }
+    
+    getVideo();
